8811b61ec79740a23072603132f16154a99ca35d
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-02-10 12:52:20 +0100

    fix oppg2 oving8

--- oving8.py
+++ oving8.py
@@ -52,24 +52,33 @@ It = Vt/Zeq
 
 printPolar(Ir, Is, It)
 
+from math import sqrt
 
 # 2.4 Kondensatorstrømmer
-Ir_xc = Ir * (Z/parallell(Z, Xc/3))
-Is_xc = Is * (Z/parallell(Z, Xc/3))
-It_xc = It * (Z/parallell(Z, Xc/3))
+Idelingsfaktor = Z/((Xc/3) + Z)
+Ir_xc_star = Ir * Idelingsfaktor 
+Ir_xc_delta = Ir_xc_star * fromPolar(1/sqrt(3), 30)
 
-printPolar(Ir_xc, Is_xc, It_xc)
+Is_xc_star = Is * Idelingsfaktor
+Is_xc_delta = Is_xc_star * fromPolar(1/sqrt(3), 30)
 
+It_xc_star = It * Idelingsfaktor
+It_xc_delta = It_xc_star * fromPolar(1/sqrt(3), 30)
+
+printPolar(Ir_xc_star, Is_xc_star, It_xc_star, Ir_xc_delta, Is_xc_delta, It_xc_delta)
+plotPolar(Ir_xc_star, Is_xc_star, It_xc_star, Ir_xc_delta, Is_xc_delta, It_xc_delta)
 
 # 2.5 Fasestrømmer
-Ir_z = Ir - Ir_xc
-Is_z = Is - Is_xc
-It_z = It - It_xc
+
+Idelingsfaktor = (Xc/3)/((Xc/3) + Z)
+Ir_z = Ir * Idelingsfaktor 
+Is_z = Is * Idelingsfaktor
+It_z = It * Idelingsfaktor 
 
 printPolar(Ir_z, Is_z, It_z)
 
 
-plotPolar(Ir, Is, It, Ir_xc, Is_xc, It_xc, Ir_z, Is_z, It_z)
+plotPolar(Ir, Is, It, Ir_xc_star, Is_xc_star, It_xc_star, Ir_z, Is_z, It_z)
 
 # 2.6 Fasespenninger
 Vr_z = Ir_z * Z
@@ -82,7 +91,7 @@ printPolar(Vr_z, Vs_z, Vt_z)
 # 2.7 Total aktiv og reaktiv effekt i last, kondensatorer og linjeimpedanser
 Sz = 3 * Z * (abs(Ir_z)**2)
 Szl = 3 * Zl * (abs(Ir)**2)
-Sxc = 3 * Xc * (abs(Ir_xc)**2)
+Sxc = 3 * Xc * (abs(Ir_xc_delta)**2)
 S = Sz + Szl + Sxc
 
 printPolar(Sz, Szl, Sxc, S)
