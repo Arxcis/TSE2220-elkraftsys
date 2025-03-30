c695c5e80162ce5c70f7f06bb83afbaea3ed762e
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-03-30 21:34:59 +0200

    crunching along on the power flow analysis... no solution yet

--- oblig-power-factory/power-flow-analysis.py
+++ oblig-power-factory/power-flow-analysis.py
@@ -1,35 +1,25 @@
-from dataclasses import dataclass
-
-@dataclass
-class Trafo:
-    S: int       # Rated power
-    X_pu: float  # short-circuit impedance per unit
-
 
 def main():
     print("main()")
-    Sbase = 40e6 # 40MVA
-    Vbase = 132e3 # 132kV
-    Zbase = Vbase**2 / Sbase
+    Sbase_40MVA = 40e6
     
-
     #
     # Step 1: Generate the Ybus matrix for the whole network with 8 buses (8x8 matrix)
     #
-    Ytrafo = lambda s, z: (z * (Sbase/s))**-1
-    YT1, YT2, YT3, YT4 = Ytrafo(z=0.03j, s=40.0e6),\
-                         Ytrafo(z=0.06j, s= 0.4e6),\
-                         Ytrafo(z=0.06j, s= 0.7e6),\
-                         Ytrafo(z=0.06j, s= 2.0e6)
-
-
-    Ycable = lambda km: ((0.11*km + 0.20j*km)/Zbase)**-1
-    YC1, YC2, YC3 = Ycable(km=0.2),\
-                    Ycable(km=2.0),\
-                    Ycable(km=1.0)
+    y_trafo = lambda s, z: (z * (Sbase_40MVA/s))**-1
+    YT1, YT2, YT3, YT4 = y_trafo(z=0.03j, s=40.0e6),\
+                         y_trafo(z=0.06j, s= 0.4e6),\
+                         y_trafo(z=0.06j, s= 0.7e6),\
+                         y_trafo(z=0.06j, s= 2.0e6)
     
-    from numpy import array
-    Ypu = array([
+
+    y_cable = lambda km, Vbase: (0.11*km/(Vbase**2/Sbase_40MVA) + 0.20j*km/(Vbase**2/Sbase_40MVA))**-1
+    YC1, YC2, YC3 = y_cable(km=0.2, Vbase=132e3),\
+                    y_cable(km=2.0, Vbase=11e3),\
+                    y_cable(km=1.0, Vbase=11e3)
+    
+    from numpy import array,abs
+    Y = array([
     # Bus   1,         2,             3,    4,              5,    6,         7,    8
         [+YT1,      -YT1,             0,    0,              0,    0,         0,    0], # 1
         [-YT1, (YT1+YC1),          -YC1,    0,              0,    0,         0,    0], # 2
@@ -40,44 +30,49 @@ def main():
         [   0,         0,             0,    0,           -YC3,    0, (YC3+YT4), -YT4], # 7
         [   0,         0,             0,    0,              0,    0,      -YT4, +YT4], # 8
     ])
-    
-    Ydiag_pu= array([
-        +YT1,
-        +YT1+YC1,
-        +YC1+YT2+YC2,
-        +YT2,
-        +YC2+YT3+YC3,
-        +YT3,
-        +YC3+YT4,
-        +YT4
-    ])
-   
-    #
-    # Step 2: Set all initial 8 bus voltages to 1pu
-    #
-    Vpu = array([1, 1, 1, 1, 1, 1, 1, 1])
+    for y in Y:
+        print(",".join(f"{y:>10.3g}" for y in abs(y)))
 
     #
-    # Step 3: Setup the loads for each bus. Could add generators as well here if present, but with opposite sign.
+    # Step 2: Setup the loads for each bus. Could add generators as well here if present, but with opposite sign.
     #
     from math import sin, acos
 
-    Sload = lambda p, cosfi: -(p + (1j*p/cosfi)*sin(acos(cosfi))) / Sbase
-    Spu = [
+    Sload = lambda p, cosfi: (p + (1j * p * sin(acos(cosfi)) / cosfi))/Sbase_40MVA
+    S = [
+        0,
+        0,
+        0,
+        -Sload(p=150e3, cosfi=0.96),
         0,\
-        0,\
-        0,\
-        Sload(p=150e3, cosfi=0.96),\
-        0,\
-        Sload(p=400e3, cosfi=0.96),\
-        0,\
-        Sload(p=1500e3, cosfi=0.96),\
+        -Sload(p=400e3, cosfi=0.96),
+        0,
+        -Sload(p=150e3, cosfi=0.96),
     ]
+    #print(",  ".join(f"{v:>10.3g}" for v in abs(S)))
 
+    #
+    # Step 3: Use Gauss-Siedel method to numerically approximate V
+    #
+    from numpy import conjugate, abs, ones
+    
+    Vbases = array([132e3, 11e3, 11e3, 230, 11e3, 230, 11e3, 230])
+    V = ones(8)
+    Yii = array([Y[i][i] for i in range(len(Y))])
 
+    for _ in range(100000):
 
-    print(Vpu)
+        I = conjugate(S) / conjugate(V)
+        YVsum = Y @ V
 
+        #print(",".join(f"{v:>10.3g}" for v in abs(YVsum)))
+
+        V = (1/Yii) * (I - (YVsum - (Yii*V)))
+
+        # Always reset slack-bus to 1pu
+        V[0] = 1
+
+        #print(",  ".join(f"{v:>10.3g}" for v in abs(V*Vbases)))
 
 if __name__ == "__main__":
     main()
