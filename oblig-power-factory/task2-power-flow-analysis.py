43b4ab9a1eea9543c929166c526dc65dd100bc0d
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-03-31 18:31:30 +0200

    task2 power flow / add 200kw to 1,2 or 3

--- oblig-power-factory/task2-power-flow-analysis.py
+++ oblig-power-factory/task2-power-flow-analysis.py
@@ -1,30 +1,37 @@
 
 def main():
-    Sbase_40MVA = 40e6
+    #
+    # Step 0: Set network Sbase
+    #
+    Sbase = 40e6
     
     #
     # Step 1: Generate the Ybus matrix for the whole network with 8 buses (8x8 matrix)
     #
-    print_title("Y admittance matrix")
-   
-    y_trafo = lambda s, z: (z * (Sbase_40MVA/s))**-1
+    def ytrafo_pu(s, z):
+        z_pu = z * (Sbase/s)
+        return 1/z_pu
 
-    YT1, YT2, YT3, YT4 = y_trafo(z=0.03j, s=40.0e6),\
-                         y_trafo(z=0.06j, s= 0.4e6),\
-                         y_trafo(z=0.06j, s= 0.7e6),\
-                         y_trafo(z=0.06j, s= 2.0e6)
+    YT1, YT2, YT3, YT4 = ytrafo_pu(z=0.03j, s=40.0e6),\
+                         ytrafo_pu(z=0.06j, s=0.4e6),\
+                         ytrafo_pu(z=0.06j, s=0.7e6),\
+                         ytrafo_pu(z=0.06j, s=2.0e6)
     
 
-    y_cable = lambda km, Vbase: (0.11*km/(Vbase**2/Sbase_40MVA) \
-                              + 0.20j*km/(Vbase**2/Sbase_40MVA))**-1
+    def ycable_pu(km, vbase):
+        zbase = vbase**2/Sbase
+        r_pu = 0.11*km / zbase
+        x_pu = 0.20j*km / zbase
 
-    YC1, YC2, YC3 = y_cable(km=0.2, Vbase=11e3),\
-                    y_cable(km=2.0, Vbase=11e3),\
-                    y_cable(km=1.0, Vbase=11e3)
+        return 1/(r_pu + x_pu)
+
+    YC1, YC2, YC3 = ycable_pu(km=0.2, vbase=11e3),\
+                    ycable_pu(km=2.0, vbase=11e3),\
+                    ycable_pu(km=1.0, vbase=11e3)
     
 
-    from numpy import array,abs
-    Y = array([
+    from numpy import array
+    Ybus = array([
     # Bus   1,         2,             3,    4,              5,    6,         7,    8
         [+YT1,      -YT1,             0,    0,              0,    0,         0,    0], # 1. bus
         [-YT1, (YT1+YC1),          -YC1,    0,              0,    0,         0,    0], # 2. bus
@@ -35,108 +42,139 @@ def main():
         [   0,         0,             0,    0,           -YC3,    0, (YC3+YT4), -YT4], # 7.
         [   0,         0,             0,    0,              0,    0,      -YT4, +YT4], # 8.
     ])
-    for y in Y:
-        print(",".join(f"{y:>10.3g}" for y in abs(y)))
 
     #
-    # Step 2: Setup the loads for each bus. Could add generators as well here if present, but with opposite sign.
+    # Step 2: Setup the loads for each bus.
+    #           * Generators + positive sign.
+    #           * Loads - negative sign
     #
-        
     from math import sin, acos
 
-    Sload = lambda p, cosfi: (p + (1j * p * sin(acos(cosfi)) / cosfi))/Sbase_40MVA
-    Sin = array([
-        0,
-        0,
-        0,
-        -Sload(p=150e3, cosfi=0.96),
-        0,
-        -Sload(p=400e3, cosfi=0.96),
-        0,
-        -Sload(p=1500e3, cosfi=0.96),
-    ])
-    
-    print_title("Bus loads")
-    print_bus_header("[VA]")
-    print_bus_values(0, Sin*Sbase_40MVA)
+    def s_pu(p, cosfi):
+        fi = acos(cosfi)
+        s = p/cosfi
+        q = 1j * s * sin(fi)
 
+        return (p + q) / Sbase
+
+    title("Task 2a): Bus voltage and power flow simulation")
+
+    load_flow(Ybus, Sbase, Sload = array([
+        0,
+        0,
+        0,
+        -s_pu(p=150e3, cosfi=0.96),
+        0,
+        -s_pu(p=400e3, cosfi=0.96),
+        0,
+        -s_pu(p=1500e3, cosfi=0.96),
+    ]))
+    
+    
+    title("Task 2b-1): Add 200kW load to Area 1")
+
+    load_flow(Ybus, Sbase, Sload = array([
+        0,
+        0,
+        0,
+        -s_pu(p=150e3, cosfi=0.96) - s_pu(p=200e3, cosfi=0.96), # <-- Add 200kw load to Area 1
+        0,
+        -s_pu(p=400e3, cosfi=0.96),
+        0,
+        -s_pu(p=1500e3, cosfi=0.96),
+    ]))
+    
+    
+    title("Task 2b-2): Add 200kW load to Area 2")
+
+    load_flow(Ybus, Sbase, Sload = array([
+        0,
+        0,
+        0,
+        -s_pu(p=150e3, cosfi=0.96),
+        0,
+        -s_pu(p=400e3, cosfi=0.96) - s_pu(p=200e3, cosfi=0.96), # <-- Add 200kw load to Area 2
+        0,
+        -s_pu(p=1500e3, cosfi=0.96),
+    ]))
+
+
+    title("Task 2b-3): Add 200kW load to Area 3")
+
+    load_flow(Ybus, Sbase, Sload = array([
+        0,
+        0,
+        0,
+        -s_pu(p=150e3, cosfi=0.96),
+        0,
+        -s_pu(p=400e3, cosfi=0.96),
+        0,
+        -s_pu(p=1500e3, cosfi=0.96) - s_pu(p=200e3, cosfi=0.96), # <-- Add 200kw load to Area 3
+    ]))
+
+
+def load_flow(Ybus, Sbase, Sload):
     #
     # Step 3: Use Gauss-Siedel method to numerically approximate V
     #
+    from numpy import conjugate, ones, sqrt, array
+    
     N = 10_000
-
-    print_title(f"Bus voltages simulation ({N} iterations)")
-    print_bus_header("[V]")
-    
-    from numpy import conjugate, abs, ones, sqrt
-    
+    Vbus = ones(8) # V = [1pu, 1pu, 1pu, 1pu, ...]
+    Yii = array([Ybus[i][i] for i in range(8)])
     Vbases = array([132e3, 11e3, 11e3, 230, 11e3, 230, 11e3, 230])
 
-    V = ones(8)
-    Yii = array([Y[i][i] for i in range(8)])
-
-    for i in range(N):
-        I = conjugate(Sin) / conjugate(V)
+    for _ in range(N):
+        I = conjugate(Sload / Vbus)
         
-        YVij = Y.dot(V)
-        YVii = Yii*V
+        YVij = Ybus.dot(Vbus)
+        YVii = Yii*Vbus
 
-        V = (1/Yii) * (I - YVij + YVii)
+        Vbus = (1/Yii) * (I - YVij + YVii)
 
         # Always reset slack-bus to 1pu
-        V[0] = 1
+        Vbus[0] = 1
         
-        #print_bus_values(i, V*Vbases)
-
     #
     # Step 4: Calculate total power flows
     #
-    Ibases = Sbase_40MVA / (V*Vbases *sqrt(3))
-    I = Y.dot(V) * Ibases
-    S = conjugate(I)*V*Vbases*sqrt(3)
-
-    # Print results
-    print_title("Bus power flows")
-    print_bus_header("")
-    print_bus_values_real("Pin[W]", Sin*Sbase_40MVA)
-    print_bus_values_imag("Qin[VAr]", Sin*Sbase_40MVA)
-    print_bus_values("Videa[V]", Vbases)
-    print_bus_values("Vsimu[V]", V*Vbases)
-    print_bus_values_real("Ireal[A]", I)
-    print_bus_values_imag("Iimag[A]", I)
-    print_bus_values_real("P[W]", S) 
-    print_bus_values_imag("Q[VAr]", S)
+    Ibases = Sbase / (Vbus*Vbases * sqrt(3))
+    I = Ybus.dot(Vbus) * Ibases
+    Sbus = conjugate(I) * Vbus*Vbases * sqrt(3)
 
 
-def print_title(title):
-        print(f"""\n\n
-{title}
------------------------------------------------------------
-""")
+    #
+    # Step 5: Print results
+    #
+    print(f"""
+    |-------------| {line()} |
+    |             | {names("Bus", 8)} |
+    |-------------| {line()} |
+    | Vinit [V]   | {abs_values(Vbases)} |
+    | Vsimu [V]   | {abs_values(Vbus*Vbases)} |
+    |-------------| {line()} |
+    | Pload [W]   | {real_values(Sload*Sbase)} |
+    | Psimu [W]   | {real_values(Sbus)} |
+    |-------------| {line()} |
+    | Qload [VAr] | {imag_values(Sload*Sbase)} |
+    | Qsimu [VAr] | {imag_values(Sbus)} |
+    |-------------| {line()} |
+    """)
 
 
-def print_bus_header(unit):
-
-    label = f"{" ":>11}"
-    print(f" {label} | {" | ".join(f"{f"Bus{i+1} {unit}":<9}" for i in range(8))}")
-    print(f" {label} | {" | ".join(f"{f"---------":<9}" for _ in range(8))}")
-
-
-def print_bus_values(label, values):
-    label = f"{label:>11}"
-    values = " | ".join(f"{v:>9.4g}" for v in abs(values))
-    print(f" {label} | {values}")
-
-def print_bus_values_real(label, values):
-    label = f"{label:>11}"
-    values = " | ".join(f"{v.real:>9.3g}" if abs(v.real) > 1e-6 else f"{0:>9.3g}" for v in values)
-    print(f" {label} | {values}")
-
-def print_bus_values_imag(label, values):
-    label = f"{label:>11}"
-    values = " | ".join(f"{v.imag:>9.3g}" if abs(v.imag) > 1e-6 else f"{0:>9.3g}" for v in values)
-    print(f" {label} | {values}")
-
+# Print helpers
+def title(text):
+    print("\n\t"+text)
+def line():
+    return " | ".join("----------" for _ in range(8))
+def names(name, count):
+    return " | ".join(f"{f"{name} {i}":<10}" for i in range(1, count+1))
+def real_values(values): 
+    return" | ".join(f"{x.real:>10.4g}" if abs(x) > 1e-6 else f"{0:>10}" for x in values)
+def imag_values(values): 
+    return" | ".join(f"{x.imag:>10.4g}" if abs(x) > 1e-6 else f"{0:>10}" for x in values)
+def abs_values(values): 
+    return" | ".join(f"{abs(x):>10.4g}" if abs(x) > 1e-6 else f"{0:>10}" for x in values)
 
 
 if __name__ == "__main__":
